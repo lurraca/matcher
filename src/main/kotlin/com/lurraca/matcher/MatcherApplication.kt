5f72bab6837a37a78837193131fde0179b6d2f70
Luis Urraca <lurraca@pivotal.io>
2020-03-07 19:34:18 +0000

    Initial Commit. Project Setup.

---
+++ src/main/kotlin/com/lurraca/matcher/MatcherApplication.kt
@@ -0,0 +1,11 @@
+package com.lurraca.matcher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatcherApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatcherApplication>(*args)
+}
---
+++ src/test/kotlin/com/lurraca/matcher/MatcherApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.lurraca.matcher
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatcherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
