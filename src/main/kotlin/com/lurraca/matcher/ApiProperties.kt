5af9026feedb238e03bd843538698bd356b9a3f4
Luis Urraca <lurraca@pivotal.io>
2020-03-08 20:55:42 +0000

    Optimize imports on ApiProperties class.

--- src/main/kotlin/com/lurraca/matcher/ApiProperties.kt
+++ src/main/kotlin/com/lurraca/matcher/ApiProperties.kt
@@ -1,12 +1,9 @@
 package com.lurraca.matcher
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
 
-@ConfigurationProperties(prefix="api")
-class ApiProperties  {
+@ConfigurationProperties(prefix = "api")
+class ApiProperties {
     lateinit var companiesEndpoint: String
     lateinit var representativesEndpoint: String
 }
