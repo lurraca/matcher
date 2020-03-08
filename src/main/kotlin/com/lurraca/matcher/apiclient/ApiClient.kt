9c433ffd15fe37ae4d61d5202a7ec2b6135aae07
Luis Urraca <lurraca@pivotal.io>
2020-03-08 05:33:40 +0000

    Adds API client thats manipulate JSON and turn into Java objects

---
+++ src/main/kotlin/com/lurraca/matcher/apiclient/ApiClient.kt
@@ -0,0 +1,76 @@
+package com.lurraca.matcher.apiclient
+import com.lurraca.matcher.AppProperties
+import com.lurraca.matcher.models.Company
+import com.lurraca.matcher.models.Contact
+import com.lurraca.matcher.models.Representative
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.io.IOException
+
+@Component
+class ApiClient {
+    @Autowired
+    private lateinit var appProperties: AppProperties
+
+    private val client = OkHttpClient()
+
+
+    fun getCompanies(): List<Company> {
+        val companies = mutableListOf<Company>()
+
+        val request = Request.Builder()
+                .url(appProperties.apiCompaniesEndpoint)
+                .build()
+        var responseJSON: JSONArray
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            responseJSON = JSONArray(response.body!!.string())
+
+            for (i in 0 until responseJSON.length()) {
+                val companyJSON = responseJSON.getJSONObject(i)
+                val companyContactJSON = companyJSON["CONTACT"] as JSONObject
+                val company = Company(
+                        companyJSON["NAME"] as String,
+                        companyJSON["ADDRESS"] as String,
+                        Contact(companyContactJSON["NAME"] as String, companyContactJSON["EMAIL"] as String, companyContactJSON["PHONE"] as String)
+                )
+                company.latitude = companyJSON["LATITUDE"] as Double
+                company.longitude = companyJSON["LONGITUDE"] as Double
+                companies.add(company)
+            }
+        }
+        return companies
+    }
+
+    fun getRepresentatives(): List<Representative> {
+        val representatives = mutableListOf<Representative>()
+
+        val request = Request.Builder()
+                .url(appProperties.apiRepresentativesEndpoint)
+                .build()
+        var responseJSON: JSONArray
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            responseJSON = JSONArray(response.body!!.string())
+
+            for (i in 0 until responseJSON.length()) {
+                val representativeJSON = responseJSON.getJSONObject(i)
+                val rep = Representative(representativeJSON["name"] as String, representativeJSON["email"] as String, representativeJSON["phone"] as String)
+                val location = (representativeJSON["location"] as String).split(", ")
+                rep.latitude = location.first().toDouble()
+                rep.longitude = location.last().toDouble()
+
+                representatives.add(rep)
+            }
+        }
+        return representatives
+    }
+}
--- src/main/kotlin/com/lurraca/matcher/controllers/OpportunityController.kt
+++ src/main/kotlin/com/lurraca/matcher/controllers/OpportunityController.kt
@@ -1,5 +1,7 @@
 package com.lurraca.matcher.controllers
 
+import com.lurraca.matcher.AppProperties
+import com.lurraca.matcher.apiclient.ApiClient
 import com.lurraca.matcher.models.Company
 import com.lurraca.matcher.models.Contact
 import com.lurraca.matcher.models.Opportunity
@@ -7,17 +9,23 @@ import com.lurraca.matcher.models.Representative
 import com.lurraca.matcher.services.OpportunityService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class OpportunityController{
+class OpportunityController {
 
     @Autowired
     lateinit var opportunityService: OpportunityService
 
+    @Autowired
+    lateinit var apiClient: ApiClient
+
     @GetMapping("/api/v1/opportunities")
-    fun opportunities() =
-            opportunityService.listOpportunities()
+    @ResponseBody
+    fun opportunities() : List<Opportunity> {
+        return opportunityService.listOpportunities(apiClient.getRepresentatives(), apiClient.getCompanies())
+    }
 
 }
 
